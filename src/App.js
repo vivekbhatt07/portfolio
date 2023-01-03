9707ce0a60ff02f53f02719f94a14227d44a2adb
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-01-03 13:19:42 +0530

    add: react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.scss";
-import { HomePage } from "./Pages";
+import { HomePage, AboutPage, ProjectPage } from "./Pages";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
-      <a href="www.google.com">Hello</a>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/About" element={<AboutPage />} />
+        <Route path="/Projects" element={<ProjectPage />} />
+      </Routes>
     </div>
   );
 }
